59b05e4f4be6cf1629819737532395f3d63e526b
Roshan Sachan <roshanjsachan@gmail.com>
2020-01-12 18:10:15 +0530

    handle infinity

--- src/services/format.js
+++ src/services/format.js
@@ -1,8 +1,12 @@
 export default (number) => {
+
     if(number!==''){
         if(Math.round(number) !== number) {
             return  number.toFixed(2);
         }
+    }
+    if(number === Infinity){
+        return  '∞';
     }
     return number;
 }
